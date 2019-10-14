c05f7825542b83027af84e1f023f51f75011aaa5
Shunyang2018 <30486093+Shunyang2018@users.noreply.github.com>
2019-10-14 16:39:37 -0700

    add another method on paired histogram

--- boxplot.py
+++ boxplot.py
@@ -55,9 +55,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.interpolate import spline
 
-df = pd.read_excel('/Users/shunyang/Downloads/qceims reference/ppt/tables.xlsx',header = 0, sheet_name = 'Sheet2')
-energy = df['number'].tolist()
-x = df['index'].tolist()
+df = pd.read_excel('/Users/shunyang/Files/qceims reference/QCEIMS_SI.xlsx',sheet_name='2-nonene conformers')
+energy = df['om2 energy (kcal/mol)'].tolist()
+x = df['Index'].tolist()
 xnew = np.linspace(1,119,300)
 energy_smooth = spline(x, energy ,xnew)
 fig = plt.figure()
@@ -65,9 +65,12 @@ ax = fig.add_subplot(111)
 #ax.plot(xnew, energy_smooth, c ='b')
 ax.plot(x, energy, c ='b')
 ax.scatter(x, energy, c ='r', linewidths=0.1,label = 'Energy of conformers')
+#ax = df.hist('om2 energy (kcal/mol)', grid=False, color='b',normed=True)
 ax.set_xlabel('Molecule',fontsize=20)
 ax.set_ylabel('Single point energy (kCal/Mol)',fontsize=15)
 ax.legend(fontsize=12)
+ax.set_xlim(0,100)
+plt.grid(ls = '--')
 plt.savefig('conformers_energy.png', dpi=400)
 plt.show()
 
@@ -77,18 +80,18 @@ from scipy import stats
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-df = pd.read_csv('/Users/shunyang/Box/qceims_input/conformer-2-nonene/qceims_gmmx_results/gmmx.csv')
+df = pd.read_excel('/Users/shunyang/Files/qceims reference/QCEIMS_SI.xlsx',sheet_name='2-nonene conformers')
 fig, axes = plt.subplots(nrows=1, ncols=1, sharex=True, sharey=True)
 
-ax = df.plot(x='number',y='Cos', kind = 'scatter',ax=axes, linewidths=0.2, fontsize=10)
+ax = df.plot(x='om2 energy (kcal/mol)',y='Cos', kind = 'scatter',ax=axes, linewidths=0.2, fontsize=10)
 plt.grid(ls = '--')
 ax.set_xlabel ('Single point energy (kCal/Mol)', fontsize=15)
 ax.set_ylabel('Cos similarity score',fontsize=15)
-ax.set_ylim(750,840)
-ax.set_xlim(0,3)
-
-plt.savefig('conformers_score.png', dpi=600)
+#ax.set_ylim(750,840)
+#ax.set_xlim(0,3)
 plt.show()
+plt.savefig('conformers_score.png', dpi=600)
+
 
 #%%
 'PHI&COS'
@@ -114,9 +117,34 @@ plt.suptitle('')
 plt.savefig('cos_phi.png', dpi=600)
 
 
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+df = pd.read_excel('/Users/shunyang/Documents/qceims reference/QCEIMS_SI.xlsx', sheet_name = 'data')
 
+df['Cos'] = df['Cos']*1000
+fig, ax = plt.subplots()
+bins = np.linspace(0, 1000, 100)
+ax.hist(df['Cos'].tolist(), bins, color='b', rwidth=0.8, alpha=0.5, label='Cos')
+ax.hist(df['Dot'].tolist(), bins, color='g', rwidth=0.8, alpha=0.5, label='Dot')
+# find minimum and maximum of xticks, so we know
+# where we should compute theoretical distribution
+xt = plt.xticks()[0]  
+xmin, xmax = min(xt), max(xt)  
+lnspc = np.linspace(xmin, xmax, len(df.Cos.tolist()))
+# lets try the normal distribution first
+m, s = stats.norm.fit(ser) # get mean and standard deviation  
+pdf_g = stats.norm.pdf(lnspc, m, s) # now get theoretical values in our interval  
+plt.plot(lnspc, pdf_g, label="Norm", color='g') # plot it
 
-
+ax.set_ylabel('Frequency ',fontsize=15)
+ax.set_xlabel('Dot similarity score',fontsize=15)
+ax.set_xlim(0,1000)
+ax.get_figure().gca().set_title("")
+plt.show()
+plt.savefig('cosall.png', dpi=600)
 
 
 
