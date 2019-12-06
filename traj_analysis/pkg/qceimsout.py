65d9de15f2e46854a5d93510f12b2ad18ff3b457
Shunyang2018 <30486093+Shunyang2018@users.noreply.github.com>
2019-12-06 15:10:53 -0800

    qceims.out

---
+++ traj_analysis/pkg/qceimsout.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  6 11:55:50 2019
+
+@author: shunyang
+"""
+
+import numpy as np
+#import tarfile
+import re
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+
+'''
+To use the code, change the path of qceims.out file, 
+the program will generate a qceims.csv file at the same path
+'''
+
+path = '/Users/shunyang/project/qceims_input/184/qceims.out'
+
+class qceimsout():
+    def __init__(self,path):
+        '''
+        get qceims.out file
+        as a list
+        '''
+        self.path = path
+        self.trajectory1 =[]
+        self.trajectory2 = []
+        self.exit = []
+        self.content = []
+#        self.calls = []
+        with open(path) as f:
+            for block in self.readblocks(f):
+                self.block = block
+                self.process(block)
+                
+        f.close()
+            
+    def readblocks(self, file):
+        block = []
+        flag = False
+        for line in file:
+            if 'ntraj' in line:
+                flag = True
+            if flag :
+                if ('normal termination of QCEIMS' in line):
+                    yield block
+                    flag = False
+                    block = []
+                block.append(line)
+
+
+    def process(self, block):
+
+#        flag= False
+        for i in range(len(block)):
+            if 'trajectory' in block[i]:
+                tmp = block[i].split()
+                self.trajectory1.append(tmp[1])
+                self.trajectory2.append(tmp[2])
+#                flag = not flag
+            if ('E X I T' in block[i])|('EXIT' in block[i]):
+                self.exit.append(block[i])
+                self.content.append(block[i-2])
+#                flag = not flag
+#        if not flag:
+#            self.re = block
+#        if len(self.trajectory1) != len(self.exit):
+#            print(block)
+#            raise SystemExit('wrong!!!')
+#            if 'QC calls' in block[i]:
+#                self.calls.append(re.findall(r'*[0-9]',block[i]))
+               
+                
+    def excel(self):
+        data = {'index1': self.trajectory1, 'index2': self.trajectory2, 
+                               'reason': self.exit, 'step   time [fs]    Epot       Ekin       Etot    error  #F   eTemp   frag. T': self.content}
+        
+        self.pd = pd.DataFrame(data)
+        self.pd.to_csv(path.split('.')[0]+'.csv')
+        return self.pd
+                    
+            
+
+
+y = qceimsout(path)
+print(len(y.trajectory1),len(y.trajectory2),len(y.exit),len(y.content))
+
+y.excel()
+
+
+
+
+
+
+
+
+
+
